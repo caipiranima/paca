6a7cd09048020ce45043a6c48ddb37fad7aae340
caipiranima <filipeamiranda@gmail.com>
2019-03-25 00:04:34 -0300

    Implementação inicial da função de adicionar animação

--- src/renderer/components/Film/FilmLibrary.vue
+++ src/renderer/components/Film/FilmLibrary.vue
@@ -1,15 +1,58 @@
 <template>
   <div class="form-wrapper">
+    <router-link to="/">Voltar</router-link>
     <h1 class="title">Filmes</h1>
     <form v-on:submit.prevent="onSubmit">
       <div class="field">
-        <label for="filmTitle" class="label"></label>
+        <label for="title" class="label"></label>
         <div class="control">
-          <input name="filmeTitle" type="text" placeholder="Title" v-model="film.title" class="input">
+          <input name="title" type="text" placeholder="Title" v-model="film.title" class="input">
         </div>
       </div>
-      <button v-if="updating" class="button">Update</button>
-      <button v-else class="button">Add</button>
+      <div class="field">
+        <label for="director" class="label"></label>
+        <div class="control">
+          <input
+            name="director"
+            type="text"
+            placeholder="Realizador(a)"
+            v-model="film.director"
+            class="input"
+          >
+        </div>
+      </div>
+      <div class="field">
+        <label for="country" class="label"></label>
+        <div class="control">
+          <input name="country" type="text" placeholder="País" v-model="film.country" class="input">
+        </div>
+      </div>
+      <div class="field">
+        <label for="synopsis" class="label"></label>
+        <div class="control">
+          <input
+            name="synopsis"
+            type="text"
+            placeholder="Sinopse"
+            v-model="film.synopsis"
+            class="input"
+          >
+        </div>
+      </div>
+      <div class="field">
+        <label for="link" class="label"></label>
+        <div class="control">
+          <input name="link" type="text" placeholder="Link" v-model="film.link" class="input">
+        </div>
+      </div>
+      <div class="field">
+        <label for="image" class="label"></label>
+        <div class="control">
+          <input name="image" type="text" placeholder="Sinopse" v-model="film.image" class="input">
+        </div>
+      </div>
+      <button v-if="updating" class="button">Atualizar</button>
+      <button v-else class="button">Adicionar</button>
     </form>
     <h3>Lista de Animações</h3>
     <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
@@ -19,7 +62,7 @@
         <th>País</th>
         <th>Sinopse</th>
         <th>Link</th>
-        <!-- <th>Imagem</th> -->
+        <th>Imagem</th>
         <td>Atualizar</td>
         <td>Excluir</td>
       </tr>
@@ -29,9 +72,15 @@
         <td>{{ f.country }}</td>
         <td>{{ f.synopsis }}</td>
         <td>{{ f.link }}</td>
-        <!-- <td><img :src="f.image" /></td> -->
-        <td v-on:click.prevent="onEdit(id)"><a>✎</a></td>
-        <td v-on:click.prevent="onDelete(id)"><a>✗</a></td>
+        <td>
+          <img :src="f.image" class="is-16by9">
+        </td>
+        <td v-on:click.prevent="onEdit(id)">
+          <a>✎</a>
+        </td>
+        <td v-on:click.prevent="onDelete(id)">
+          <a>✗</a>
+        </td>
       </tr>
     </table>
   </div>
@@ -44,9 +93,31 @@ export default {
       updating: false,
       films: this.$store.getters.films,
       film: {
-        title: ''
+        title: "",
+        direcotr: "",
+        country: "",
+        synopsis: "",
+        link: "",
+        image: ""
       }
     };
+  },
+  methods: {
+    onSubmit() {
+      if (this.updating) {
+        this.onUpdate();
+        return;
+      }
+      this.$store.dispatch('addFilm', this.film);
+      this.film = {
+        title: "",
+        direcotr: "",
+        country: "",
+        synopsis: "",
+        link: "",
+        image: ""
+      };
+    }
   }
 };
 </script>
--- src/renderer/store/modules/Library.js
+++ src/renderer/store/modules/Library.js
@@ -1,34 +1,39 @@
-const state = {
-  films: [
-    {
-      id: 1,
-      title: 'Pinocchio',
-      director: 'Gianluigi Toccafondo',
-      country: 'Itália',
-      synopsis: 'Um filme',
-      link: 'https://vimeo.com/235527477',
-      image: 'https://i.vimeocdn.com/video/657420157.jpg?mw=800&mh=450'
-    },
-    {
-      id: 2,
-      title: 'La Coda',
-      director: 'Gianluigi Toccafondo',
-      country: 'Itália',
-      synopsis: 'Um filme',
-      link: 'https://vimeo.com/235343086',
-      image: 'https://i.vimeocdn.com/video/657160343.jpg?mw=800&mh=450'
-    },
-    {
-      id: 3,
-      title: 'La Pista',
-      director: 'Gianluigi Toccafondo',
-      country: 'Itália',
-      synopsis: 'Um filme',
-      link: 'https://vimeo.com/235321322',
-      image: 'https://i.vimeocdn.com/video/657124624.jpg?mw=800&mh=450'
-    }
-  ]
+function loadInitialState() {
+  return {
+    films: [
+      {
+        id: 1,
+        title: 'Pinocchio',
+        director: 'Gianluigi Toccafondo',
+        country: 'Itália',
+        synopsis: 'Um filme',
+        link: 'https://vimeo.com/235527477',
+        image: 'https://i.vimeocdn.com/video/657420157.jpg?mw=800&mh=450'
+      },
+      {
+        id: 2,
+        title: 'La Coda',
+        director: 'Gianluigi Toccafondo',
+        country: 'Itália',
+        synopsis: 'Um filme',
+        link: 'https://vimeo.com/235343086',
+        image: 'https://i.vimeocdn.com/video/657160343.jpg?mw=800&mh=450'
+      },
+      {
+        id: 3,
+        title: 'La Pista',
+        director: 'Gianluigi Toccafondo',
+        country: 'Itália',
+        synopsis: 'Um filme',
+        link: 'https://vimeo.com/235321322',
+        image: 'https://i.vimeocdn.com/video/657124624.jpg?mw=800&mh=450'
+      }
+    ],
+    lastFilmID: 3
+  }
 }
+
+const state = loadInitialState()
 
 const getters = {
   films() {
@@ -36,7 +41,22 @@ const getters = {
   }
 }
 
+const mutations = {
+  ADD_FILM(state, film) {
+    film.id = state.lastFilmID++
+    state.films.push(film)
+  }
+}
+
+const actions = {
+  addFilm({ commit }, film) {
+    commit('ADD_FILM', film)
+  }
+}
+
 export default {
   state,
-  getters
+  getters,
+  mutations,
+  actions
 }
